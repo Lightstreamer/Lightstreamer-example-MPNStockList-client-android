279810ba07ede32d1ef42510f5a2e2aa426a8db4
mone <simone.fabiano@gmail.com>
2014-05-30 10:56:09 +0200

    introduce actionbar and prepare to style the StocksFragment

--- src/com/lightstreamer/demo/stocklistdemo_advanced/StockListDemo.java
+++ src/com/lightstreamer/demo/stocklistdemo_advanced/StockListDemo.java
@@ -16,16 +16,24 @@
 package com.lightstreamer.demo.stocklistdemo_advanced;
 
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 
-public class StockListDemo extends FragmentActivity implements StocksFragment.onStockSelectedListener {
+public class StockListDemo extends ActionBarActivity implements StocksFragment.onStockSelectedListener {
 
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.HONEYCOMB) {
+            hideActionBarTitle();//TODO do it with styles
+        }
+        
         setContentView(R.layout.stocks);
 
         if (findViewById(R.id.fragment_container) != null) {
@@ -41,33 +49,48 @@ public class StockListDemo extends FragmentActivity implements StocksFragment.on
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, firstFragment).commit();
         }
+        
+        
     }
-	 
-	 
-	 public void onStockSelected(int item) {
+    
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void hideActionBarTitle() {
+        getActionBar().setDisplayShowTitleEnabled(false);
+    }
 
-	        DetailsFragment detailsFrag = (DetailsFragment)
-	                getSupportFragmentManager().findFragmentById(R.id.details_fragment);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+     
+     public void onStockSelected(int item) {
 
-	        if (detailsFrag != null) {
-	          
-	        	detailsFrag.updateStocksView(item);
+            DetailsFragment detailsFrag = (DetailsFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.details_fragment);
 
-	        } else {
-	            // If the frag is not available, we're in the one-pane layout and must swap frags...
+            if (detailsFrag != null) {
+              
+                detailsFrag.updateStocksView(item);
 
-	            // Create fragment and give it an argument for the selected article
-	        	DetailsFragment newFragment = new DetailsFragment();
-	            Bundle args = new Bundle();
-	            args.putInt(DetailsFragment.ARG_ITEM, item);
-	            newFragment.setArguments(args);
-	            
-	            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-	            transaction.replace(R.id.fragment_container, newFragment);
-	            transaction.addToBackStack(null);
-	            transaction.commit();
-	        }
-	    }
+            } else {
+                // If the frag is not available, we're in the one-pane layout and must swap frags...
 
-	
+                // Create fragment and give it an argument for the selected article
+                DetailsFragment newFragment = new DetailsFragment();
+                Bundle args = new Bundle();
+                args.putInt(DetailsFragment.ARG_ITEM, item);
+                newFragment.setArguments(args);
+                
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        }
+
+    
 }
--- src/com/lightstreamer/demo/stocklistdemo_advanced/StocksFragment.java
+++ src/com/lightstreamer/demo/stocklistdemo_advanced/StocksFragment.java
@@ -19,25 +19,34 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StocksFragment extends ListFragment {
-	
-	onStockSelectedListener listener;
-	
-	public interface onStockSelectedListener {
+    
+    onStockSelectedListener listener;
+    
+    public interface onStockSelectedListener {
         /** Called by HeadlinesFragment when a list item is selected */
         public void onStockSelected(int item);
     }
-	
-	private final static String[] items = {"item1", "item2", "item3",
-	        "item4", "item5", "item6", "item7", "item8", "item9", "item10",
-	        "item11", "item12", "item13", "item14", "item15"};
+    
+    private final static String[] items = {"item1", "item2", "item3",
+            "item4", "item5", "item6", "item7", "item8", "item9", "item10",
+            "item11", "item12", "item13", "item14", "item15"};
 
-	 
-	@Override
+     
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.list_view, container, false);
+    }
+    
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -46,9 +55,9 @@ public class StocksFragment extends ListFragment {
 
         setListAdapter(new ArrayAdapter<String>(getActivity(), layout, items));
     }
-	
-	
-	@Override
+    
+    
+    @Override
     public void onStart() {
         super.onStart();
 
@@ -56,15 +65,15 @@ public class StocksFragment extends ListFragment {
             getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         }
     }
-	
-	@Override
+    
+    @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
         // This makes sure that the container activity has implemented
         // the callback interface. If not, it throws an exception.
         try {
-        	listener = (onStockSelectedListener) activity;
+            listener = (onStockSelectedListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnHeadlineSelectedListener");
@@ -74,7 +83,7 @@ public class StocksFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         // Notify the parent activity of selected item
-    	listener.onStockSelected(position+1);
+        listener.onStockSelected(position+1);
         
         // Set the item as checked to be highlighted when in two-pane layout
         getListView().setItemChecked(position, true);
