896cb9c681930973322cfc333f88b5ca5aa2ec7d
mone <simone.fabiano@gmail.com>
2014-08-18 16:06:09 +0200

    add the "about" dialog

--- src/com/lightstreamer/demo/stocklistdemo_advanced/StockListDemo.java
+++ src/com/lightstreamer/demo/stocklistdemo_advanced/StockListDemo.java
@@ -27,13 +27,17 @@ import com.lightstreamer.ls_client.mpn.MpnRegistrationIdStatus;
 import com.lightstreamer.ls_client.mpn.MpnRegistrationListener;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -50,6 +54,7 @@ public class StockListDemo extends ActionBarActivity implements StocksFragment.o
     private static final String TAG = "StockListDemo";
    
     private boolean pnEnabled = false;
+    
     
     private Handler handler;
 
@@ -128,6 +133,7 @@ public class StockListDemo extends ActionBarActivity implements StocksFragment.o
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, firstFragment).commit();
         } 
+        
         
     }
     
@@ -236,6 +242,10 @@ public class StockListDemo extends ActionBarActivity implements StocksFragment.o
             userDisconnect = false;
             supportInvalidateOptionsMenu();
             this.start();
+            return true;
+        } else if (itemId == R.id.about) {
+            new AboutDialog().show(getSupportFragmentManager(), null);
+            
             return true;
         } else {
             return super.onOptionsItemSelected(item);
@@ -351,6 +361,20 @@ public class StockListDemo extends ActionBarActivity implements StocksFragment.o
         }
         
     }
+    
+    public class AboutDialog extends DialogFragment {
+        
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            builder.setView(inflater.inflate(R.layout.dialog_about, null));
+            return builder.create();
+        }
+        
+    }
+    
+    
 
     @Override
     public void start() {
